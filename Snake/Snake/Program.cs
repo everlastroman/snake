8c2899fec8d8cfcaa64be95290894065ee897da8
everlast_roman <everlast.roman@gmail.com>
2016-12-14 19:51:27 +0100

    Draw Klass

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,25 +10,31 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 1;
+            p1.sym = '*';
+            p1.Draw();
+
+            Point p2 = new Point();
+            p2.x = 2;
+            p2.y = 2;
+            p2.sym = '^';
+            p2.Draw();
+
+            /*int x1 = 1;
             int y1 = 3;
             char sym1 = '*';
 
-            Draw(x1, y1, sym1);
+            Draw(x1, y1, sym1);*/
 
             int x2 = 5;
             int y2 = 3;
             char sym2 = '^';
 
-            Draw(x2, y2, sym2);
+           
 
             Console.ReadLine();
-        }
-        static void Draw( int x,int y,char sym )
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
-
         }
     }
 }
