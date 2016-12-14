8b36c53a528898067de1f92a2118f4b422b58c4b
everlast_roman <everlast.roman@gmail.com>
2016-12-14 20:02:29 +0100

    COnstruct

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,29 +10,11 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point();
-            p1.x = 1;
-            p1.y = 1;
-            p1.sym = '*';
+            Point p1 = new Point(1, 2, '&');
             p1.Draw();
 
-            Point p2 = new Point();
-            p2.x = 2;
-            p2.y = 2;
-            p2.sym = '^';
+            Point p2 = new Point(2, 2, '&');
             p2.Draw();
-
-            /*int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';
-
-            Draw(x1, y1, sym1);*/
-
-            int x2 = 5;
-            int y2 = 3;
-            char sym2 = '^';
-
-           
 
             Console.ReadLine();
         }
